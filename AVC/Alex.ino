d40bb33152263654c83a45dd5b85247376bd8179
winston-lim <winston_lim1@hotmail.com>
2022-04-10 22:00:32 +0800

    Update AVC with speed and direction calibrations

--- AVC/Alex.ino
+++ AVC/Alex.ino
@@ -1,11 +1,9 @@
-#include <serialize.h>
+#include "serialize.h"
 #include <stdarg.h>
 #include "packet.h"
 #include "constants.h"
+#include <math.h>
 
-/*
- * Alex's configuration constants
- */
 typedef enum
 {
   STOP = 0,
@@ -17,29 +15,46 @@ typedef enum
 
 volatile TDirection dir = STOP;
 
+/*
+   Alex's configuration constants
+*/
+
 // Number of ticks per revolution from the
 // wheel encoder.
 
-#define COUNTS_PER_REV 165
-#define WHEEL_CIRC 22
+#define COUNTS_PER_REV 160
 
 // Wheel circumference in cm.
 // We will use this to calculate forward/backward distance traveled
 // by taking revs * WHEEL_CIRC
 
+#define WHEEL_CIRC 20
+
 // Motor control pins. You need to adjust these till
 // Alex moves in the correct direction
-#define LF 6  // Left forward pin DIFF1
-#define LR 5  // Left reverse pin DIFF2
+#define LF 6  // Left forward pin
+#define LR 5  // Left reverse pin
 #define RF 10 // Right forward pin
-#define RR 11 // Right reverse pin
+#define RR 9  // Right reverse pin
+
+// Alex's length and breadth in cm
+#define ALEX_LENGTH 4
+#define ALEX_BREADTH 6
+
+// PI, for calculating turn in circumference
+#define PI 3.141592654
+
+// Alex's diagonal. We compute and store this once since it is
+// expensive to compute and doesn't really change.
+float AlexDiagonal = 0.0; // to change
+
+// Alex's turning circumference, calculated once
+float AlexCirc = 0.0; // to change
 
 /*
- *    Alex's State Variables
- */
+      Alex's State Variables
+*/
 
-// Store the ticks from Alex's left and
-// right encoders.
 // Store the ticks from Alex's left and
 // right encoders.
 volatile unsigned long leftForwardTicks;
@@ -62,15 +77,19 @@ volatile unsigned long rightRevs;
 volatile unsigned long forwardDist;
 volatile unsigned long reverseDist;
 
-// Variables to keep track of whether we have moved a commanded distance
+// Variable to keep track of whether we have moved a commanded distance
 unsigned long deltaDist;
 unsigned long newDist;
 
+// Variables to keep track of our turning angle
+unsigned long deltaTicks;
+unsigned long targetTicks;
+
 /*
- *
- * Alex Communication Routines.
- *
- */
+
+   Alex Communication Routines.
+
+*/
 
 TResult readPacket(TPacket *packet)
 {
@@ -133,11 +152,20 @@ void dbprint(char *format, ...)
   sendMessage(buffer);
 }
 
+void print_input(float param1, float param2)
+{
+  char _param1[11];
+  char _param2[11];
+  String(param1).toCharArray(_param1, 5);
+  String(param2).toCharArray(_param2, 5);
+  dbprint(_param1);
+  dbprint(_param2);
+}
+
 void sendBadPacket()
 {
   // Tell the Pi that it sent us a packet with a bad
   // magic number.
-
   TPacket badPacket;
   badPacket.packetType = PACKET_TYPE_ERROR;
   badPacket.command = RESP_BAD_PACKET;
@@ -194,10 +222,10 @@ void sendResponse(TPacket *packet)
 }
 
 /*
- * Setup and start codes for external interrupts and
- * pullup resistors.
- *
- */
+   Setup and start codes for external interrupts and
+   pullup resistors.
+
+*/
 // Enable pull up resistors on pins 2 and 3
 void enablePullups()
 {
@@ -213,47 +241,41 @@ void enablePullups()
 // Functions to be called by INT0 and INT1 ISRs.
 void leftISR()
 {
-  if (dir == FORWARD)
+  switch (dir)
   {
+  case FORWARD:
     leftForwardTicks++;
     forwardDist = (unsigned long)((float)leftForwardTicks / COUNTS_PER_REV * WHEEL_CIRC);
-  }
-  if (dir == BACKWARD)
-  {
+    break;
+  case BACKWARD:
     leftReverseTicks++;
     reverseDist = (unsigned long)((float)leftReverseTicks / COUNTS_PER_REV * WHEEL_CIRC);
-  }
-  if (dir == LEFT)
-  {
+    break;
+  case LEFT:
     leftReverseTicksTurns++;
-  }
-  if (dir == RIGHT)
-  {
+    break;
+  case RIGHT:
     leftForwardTicksTurns++;
+    break;
   }
 }
 
 void rightISR()
 {
-  if (dir == FORWARD)
+  switch (dir)
   {
+  case FORWARD:
     rightForwardTicks++;
-    // DIFF3
-    forwardDist = (unsigned long)((float)rightForwardTicks / COUNTS_PER_REV * WHEEL_CIRC);
-  }
-  if (dir == BACKWARD)
-  {
+    break;
+  case BACKWARD:
     rightReverseTicks++;
-    // DIFF4
-    reverseDist = (unsigned long)((float)rightReverseTicks / COUNTS_PER_REV * WHEEL_CIRC);
-  }
-  if (dir == LEFT)
-  {
+    break;
+  case LEFT:
     rightForwardTicksTurns++;
-  }
-  if (dir == RIGHT)
-  {
+    break;
+  case RIGHT:
     rightReverseTicksTurns++;
+    break;
   }
 }
 
@@ -261,9 +283,6 @@ void rightISR()
 // for falling edge triggered. Use bare-metal.
 void setupEINT()
 {
-  // Use bare-metal to configure pins 2 and 3 to be
-  // falling edge triggered. Remember to enable
-  // the INT0 and INT1 interrupts.
   // Use bare-metal to configure pins 2 and 3 to be
   // falling edge triggered. Remember to enable
   // the INT0 and INT1 interrupts.
@@ -287,9 +306,9 @@ ISR(INT1_vect)
 // Implement INT0 and INT1 ISRs above.
 
 /*
- * Setup and start codes for serial communications
- *
- */
+   Setup and start codes for serial communications
+
+*/
 // Set up the serial connection. For now we are using
 // Arduino Wiring, you will replace this later
 // with bare-metal code.
@@ -333,9 +352,9 @@ void writeSerial(const char *buffer, int len)
 }
 
 /*
- * Alex's motor drivers.
- *
- */
+   Alex's motor drivers.
+
+*/
 
 // Set up Alex's motors. Right now this is empty, but
 // later you will replace it with code to set up the PWMs
@@ -343,11 +362,11 @@ void writeSerial(const char *buffer, int len)
 void setupMotors()
 {
   /* Our motor set up is:
-   *    A1IN - Pin 5, PD5, OC0B
-   *    A2IN - Pin 6, PD6, OC0A
-   *    B1IN - Pin 10, PB2, OC1B
-   *    B2In - pIN 11, PB3, OC2A
-   */
+        A1IN - Pin 5, PD5, OC0B
+        A2IN - Pin 6, PD6, OC0A
+        B1IN - Pin 10, PB2, OC1B
+        B2In - pIN 11, PB3, OC2A
+  */
 }
 
 // Start the PWM for Alex's motors.
@@ -376,29 +395,56 @@ int pwmVal(float speed)
 // continue moving forward indefinitely.
 void forward(float dist, float speed)
 {
+  // Code to tell us how far to move
   if (dist > 0)
   {
-    deltaDist = dist;
+    deltaDist = (int)(dist * 0.78);
   }
   else
   {
-    deltaDist = 999999;
+    deltaDist = 9999999;
   }
   newDist = forwardDist + deltaDist;
   dir = FORWARD;
   int val = pwmVal(speed);
-  // For now we will ignore dist and move
-  // forward indefinitely. We will fix this
-  // in Week 9.
+  if (dist == 10 && speed == 70)
+  {
+    analogWrite(RF, (int)(val * 0.72));
+    analogWrite(LF, (int)(val));
+    analogWrite(LR, 0);
+    analogWrite(RR, 0);
+  }
+  else if (dist == 10 && speed == 90)
+  {
+    analogWrite(RF, (int)(val));
+    analogWrite(LF, (int)(val));
+    analogWrite(LR, 0);
+    analogWrite(RR, 0);
+  }
+  else if (dist == 5)
+  {
+    analogWrite(RF, (int)(val * 0.72));
+    analogWrite(LF, (int)(val));
+    analogWrite(LR, 0);
+    analogWrite(RR, 0);
+  }
 
-  // LF = Left forward pin, LR = Left reverse pin
-  // RF = Right forward pin, RR = Right reverse pin
-  // This will be replaced later with bare-metal code.
+  else if (dist == 40 && speed == 90)
+  {
+    analogWrite(RF, (int)(val));
+    analogWrite(LF, (int)(val));
+    analogWrite(LR, 0);
+    analogWrite(RR, 0);
+  }
 
-  analogWrite(LF, val);
-  analogWrite(RF, val);
-  analogWrite(LR, 0);
-  analogWrite(RR, 0);
+  else
+  {
+
+    analogWrite(RF, (int)(val * 0.72));
+    analogWrite(LF, (int)(val));
+    analogWrite(LR, 0);
+    analogWrite(RR, 0);
+  }
 }
 
 // Reverse Alex "dist" cm at speed "speed".
@@ -408,33 +454,57 @@ void forward(float dist, float speed)
 // continue reversing indefinitely.
 void reverse(float dist, float speed)
 {
-  if (dist > 0)
+  // Code to rell us how far to move
+  if (dist == 0)
   {
-    deltaDist = dist;
+    deltaDist = 9999999;
   }
   else
   {
-    deltaDist = 999999;
+    deltaDist = dist;
   }
-  dir = BACKWARD;
   newDist = reverseDist + deltaDist;
+  dir = BACKWARD;
   int val = pwmVal(speed);
+  if (dist == 10)
+  {
+    analogWrite(RR, (int)(val * 0.69));
+    analogWrite(LR, (int)(val));
+    analogWrite(LF, 0);
+    analogWrite(RF, 0);
+  }
 
-  // For now we will ignore dist and
-  // reverse indefinitely. We will fix this
-  // in Week 9.
+  else if (dist == 5)
+  {
+    analogWrite(RR, (int)(val * 0.78));
+    analogWrite(LR, (int)(val));
+    analogWrite(LF, 0);
+    analogWrite(RF, 0);
+  }
 
-  // LF = Left forward pin, LR = Left reverse pin
-  // RF = Right forward pin, RR = Right reverse pin
-  // This will be replaced later with bare-metal code.
-  analogWrite(LR, val);
-  analogWrite(RR, val);
-  analogWrite(LF, 0);
-  analogWrite(RF, 0);
+  else
+  {
+    analogWrite(RR, (int)(val * 0.78));
+    analogWrite(LR, (int)(val));
+    analogWrite(LF, 0);
+    analogWrite(RF, 0);
+  }
 }
 
-// Turn Alex left "ang" degrees at speed "speed".
-// "speed" is expressed as a percentage. E.g. 50 is
+// Function to estimate the number of wheel ticks needed to turn an angle
+unsigned long computeDeltaTicks(float ang)
+{
+  // We will assume that angular distance moved = linerar distance moved in one wheel
+  // revolution. This is (probably) incorrect but simplifies calculation.
+  // # of wheel revs to make one full 360 turn is AlexCirc / WHEEL_CIRC
+  // This is for 360 degrees. For ang degrees it will be (ang * AlexCirc) / (360.0 * WHEEL_CIRC)
+  // To convert to ticks, we multiply by COUNTS_PER_REV
+
+  unsigned long ticks = (unsigned long)((ang * AlexCirc * COUNTS_PER_REV) / (360.0 * WHEEL_CIRC));
+
+  return ticks;
+}
+
 // turn left at half speed.
 // Specifying an angle of 0 degrees will cause Alex to
 // turn left indefinitely.
@@ -443,12 +513,39 @@ void left(float ang, float speed)
   dir = LEFT;
   int val = pwmVal(speed);
 
+  if (ang == 15)
+  {
+    ang *= 1.52;
+  }
+
+  if (ang == 0)
+  {
+    deltaTicks = 9999999;
+  }
+  else
+  {
+    if (ang == 90)
+    {
+      deltaTicks = 6 * computeDeltaTicks(15 * 1.52);
+    }
+    else if (ang == 360)
+    {
+      deltaTicks = 24 * computeDeltaTicks(15 * 1.52);
+    }
+    else
+    {
+      deltaTicks = computeDeltaTicks(ang);
+    }
+  }
+
+  targetTicks = leftReverseTicksTurns + deltaTicks;
+
   // For now we will ignore ang. We will fix this in Week 9.
   // We will also replace this code with bare-metal later.
   // To turn left we reverse the left wheel and move
   // the right wheel forward.
   analogWrite(LR, val);
-  analogWrite(RF, val);
+  analogWrite(RF, (int)val * 0.85);
   analogWrite(LF, 0);
   analogWrite(RR, 0);
 }
@@ -463,11 +560,37 @@ void right(float ang, float speed)
   dir = RIGHT;
   int val = pwmVal(speed);
 
+  if (ang == 15)
+  {
+    ang *= 1.33;
+  }
+
+  if (ang == 0)
+  {
+    deltaTicks = 9999999;
+  }
+  else
+  {
+    if (ang == 90)
+    {
+      deltaTicks = 6 * computeDeltaTicks(15 * 1.33);
+    }
+    else if (ang == 360)
+    {
+      deltaTicks = 24 * computeDeltaTicks(15 * 1.33);
+    }
+    else
+    {
+      deltaTicks = computeDeltaTicks(ang);
+    }
+  }
+
+  targetTicks = leftForwardTicksTurns + deltaTicks;
   // For now we will ignore ang. We will fix this in Week 9.
   // We will also replace this code with bare-metal later.
   // To turn right we reverse the right wheel and move
   // the left wheel forward.
-  analogWrite(RR, val);
+  analogWrite(RR, (int)val * 0.78);
   analogWrite(LF, val);
   analogWrite(LR, 0);
   analogWrite(RF, 0);
@@ -476,18 +599,31 @@ void right(float ang, float speed)
 // Stop Alex. To replace with bare-metal code later.
 void stop()
 {
-  dir == STOP; // DIFF5
+  dir = STOP;
+
   analogWrite(LF, 0);
   analogWrite(LR, 0);
   analogWrite(RF, 0);
   analogWrite(RR, 0);
 }
+// Stop Alex. To replace with bare-metal code later.
+void stop_right_first()
+{
+  dir = STOP;
+  analogWrite(RF, 0);
+  delay(1);
+  analogWrite(LF, 0);
+  analogWrite(LR, 0);
+
+  analogWrite(RR, 0);
+}
 
 /*
- * Alex's setup and run codes
- *
- */
-// Clears all counters
+   Alex's setup and run codes
+
+*/
+
+// Clears all our counters
 void clearCounters()
 {
   leftForwardTicks = 0;
@@ -503,6 +639,7 @@ void clearCounters()
   forwardDist = 0;
   reverseDist = 0;
 }
+
 // Clears one particular counter
 void clearOneCounter(int which)
 {
@@ -597,6 +734,10 @@ void setup()
 {
   // put your setup code here, to run once:
 
+  // Compute the diagonal and circumference
+  AlexDiagonal = sqrt((ALEX_LENGTH * ALEX_LENGTH) + (ALEX_BREADTH * ALEX_BREADTH));
+  AlexCirc = PI * AlexDiagonal;
+
   cli();
   setupEINT();
   setupSerial();
@@ -635,7 +776,8 @@ void loop()
 
   // Uncomment the code below for Step 2 of Activity 3 in Week 8 Studio 2
 
-  // forward(0, 75);
+  // forward(0, 50);
+  // analogWrite(RF, pwmVal(80));
 
   // Uncomment the code below for Week 9 Studio 2
 
@@ -661,7 +803,7 @@ void loop()
       {
         deltaDist = 0;
         newDist = 0;
-        stop();
+        stop_right_first();
       }
     }
     else if (dir == BACKWARD)
@@ -678,6 +820,40 @@ void loop()
       deltaDist = 0;
       newDist = 0;
       stop();
+    }
+  }
+
+  if (deltaTicks > 0)
+  {
+    if (dir == LEFT)
+    {
+      if (leftReverseTicksTurns >= targetTicks)
+      {
+        deltaTicks = 0;
+        targetTicks = 0;
+        stop();
+      }
+    }
+    else
+    {
+      if (dir == RIGHT)
+      {
+        if (leftForwardTicksTurns >= targetTicks)
+        {
+          deltaTicks = 0;
+          targetTicks = 0;
+          stop();
+        }
+      }
+      else
+      {
+        if (dir == STOP)
+        {
+          deltaTicks = 0;
+          targetTicks = 0;
+          stop();
+        }
+      }
     }
   }
 }
